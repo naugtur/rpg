611debe7e25ced79c7c179089f579892d0f15c6d
Naugtur <naugtur@gmail.com>
2018-08-13 02:02:47 +0200

    v1

---
+++ tools/wrap.js
@@ -0,0 +1,28 @@
+const before = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>SciFi o odnajdywaniu sensu swojej pracy</title>
+  <link rel="stylesheet" href="./tools/style.css">
+</head>
+<body>
+<section class="cover">
+  <h1 id="start" class="title">Groza bezcelowości</h1>
+  <h4>Scenariusz grozy w klimacie Sci-Fi / Alien</h4>
+  <p class="punchline">Rozważanie sensu wykonywanej pracy. Nie po to uciekam w RPG...</p>
+</section>
+<article>
+`;
+
+const after = `
+</article>
+</body>
+</html>
+`;
+
+process.stdout.write(before)
+process.stdin.pipe(process.stdout)
+process.stdin.on('end',_=>process.stdout.write(after))
